79e357ef7975936104e9d68e82649bd21b431d08
Akanksha Omprakash Agarwal <akanksha.agarwal@glance.com>
2022-08-04 00:30:33 +0530

    more commands

---
+++ commands/add_subscription.py
@@ -0,0 +1,29 @@
+import utils.util
+from commands.command_executor import CommandExecutor
+
+
+class AddSubscription(CommandExecutor):
+    COMMAND_NAME = "ADD_SUBSCRIPTION"
+
+    def validate(self, command):
+        date = self._active_subscription_plan_service.get_date()
+        categories = self._active_subscription_plan_service.get_subscription()
+        category, plan_type = command.params
+        if utils.util.subscription_date_not_set(date):
+            self._output_printer.add_subscription_failed()
+            self._output_printer.invalid_date()
+            return False
+        elif utils.util.category_already_subscribed(categories, category):
+            self._output_printer.add_subscription_failed()
+            self._output_printer.duplicate_category()
+            return False
+        else:
+            return True
+
+    def execute(self, command):
+        self._active_subscription_plan_service.set_subscription(command)
+
+    def __init__(self, subscription_plan_service, output_printer, active_subscription_plan_service):
+        self._subscription_plan_service = subscription_plan_service
+        self._output_printer = output_printer
+        self._active_subscription_plan_service = active_subscription_plan_service
---
+++ commands/print_renewal_details.py
@@ -0,0 +1,39 @@
+import utils.util
+from commands.command_executor import CommandExecutor
+from entity.category import Category
+from entity.plan_name import PlanName
+
+
+class PrintRenewalDetails(CommandExecutor):
+    COMMAND_NAME = "PRINT_RENEWAL_DETAILS"
+
+    def validate(self, command):
+        categories = self._active_subscription_plan_service.get_subscription()
+        if not categories:
+            self._output_printer.subscription_not_found()
+            return False
+        else:
+            return True
+
+    def execute(self, command):
+        active_subscriptions = self._active_subscription_plan_service.get_subscription()
+        subscription_start_date = self._active_subscription_plan_service.get_date()
+        renewal_amount = 0
+
+        for category, plan_name in active_subscriptions.items():
+            plan = self.__fetch_duration_for_active_subscription(category, plan_name)
+            duration_unit, duration_count, plan_amount = plan.duration_unit, plan.duration_count, plan.amount
+            date = utils.util.get_renewal_date(subscription_start_date, duration_unit, duration_count)
+            renewal_amount += plan_amount
+            self._output_printer.print_renewal_details(category + " " + str(date))
+        self._output_printer.print_renewal_amount(renewal_amount)
+
+    def __fetch_duration_for_active_subscription(self, category, plan_name):
+        category, plan_name = Category[category], PlanName[plan_name]
+        subscription_plan = self._subscription_plan_service.get_subscription_by_category_and_plan_type(category, plan_name)
+        return subscription_plan
+
+    def __init__(self, subscription_plan_service, output_printer, active_subscription_plan_service):
+        self._subscription_plan_service = subscription_plan_service
+        self._output_printer = output_printer
+        self._active_subscription_plan_service = active_subscription_plan_service
